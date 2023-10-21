8cc51e6b45bf122f18f01f3fbab83e300122b8c5
ebai alice <alice.ebai@student.howest.be>
2023-10-21 14:53:44 +0200

    Remove form action attribute in HTML

--- challenges_CSRF.php
+++ challenges_CSRF.php
@@ -6,7 +6,7 @@
 <body>
     <h1>CSRF Attack Page</h1>
 
-    <form action="http://example.com/process.php" method="post">
+    <form action="challenges_CSRF.php" method="post">
         <input type="hidden" name="action" value="transfer">
         <input type="hidden" name="amount" value="9999">
         <input type="submit" value="Transfer $9999">
@@ -20,12 +20,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['action'] === 'transfer') {
         // Check if the user is logged in (you can implement your authentication logic here)
         if (isset($_SESSION['user'])) {
-            // Process the fund transfer
+            // Implement secure fund transfer logic here
             $amount = $_POST['amount'];
-            
-            // You should implement a secure way to handle the transfer
-
-            echo "Transfer of $amount was successful.";
+            // You should validate and sanitize user input, verify authorization, etc.
+            // For this example, we will just simulate a response
+            echo "Transfer initiated. Please wait for confirmation.";
         } else {
             echo "You are not logged in.";
         }
