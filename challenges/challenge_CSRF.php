6b782443ebe7af01b713bdcaffa4f45648aef64d
ebai alice <alice.ebai@student.howest.be>
2023-11-09 18:00:07 +0100

    solution to challenge 4

--- challenges/challenge_CSRF.php
+++ challenges/challenge_CSRF.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +8,7 @@
 <body>
     <h1>CSRF Attack Page</h1>
 
-    <form action="challenge_CSRF.php" method="post">
+<form action="challenge_CSRF.php" method="post">
         <input type="hidden" name="action" value="transfer">
         <input type="hidden" name="amount" value="9999">
         <button type="submit">Transfer $9999</button>
@@ -16,19 +17,17 @@
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($_POST['action'] === 'transfer') {
-        // Check if the user is logged in (you can implement your authentication logic here)
-        if (isset($_SESSION['user'])) {
-            // Implement secure fund transfer logic here
-            $amount = $_POST['amount'];
+    // Check if the user is logged in (you can implement your authentication logic here)
+    if (isset($_SESSION['user'])) {
+        if ($_POST['action'] === 'transfer') {
             // You should validate and sanitize user input, verify authorization, etc.
             // For this example, we will just simulate a response
             echo "Transfer initiated. Please wait for confirmation.";
         } else {
-            echo "You are not logged in.";
+            echo "Invalid action.";
         }
     } else {
-        echo "Invalid action.";
+        echo "You are not logged in.";
     }
 } else {
     echo "Invalid request method.";
