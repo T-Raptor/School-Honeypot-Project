d3a9ee8f28ffc1362c9da5c51174388c4d61615b
Popal Ares <ares.popal@student.howest.be>
2023-11-21 06:27:04 +0100

    user with record in database sees correct completion

--- challenges.php
+++ challenges.php
@@ -6,11 +6,21 @@ session_set_cookie_params([
 
 session_start();
 
-// Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.html");
     exit();
 }
+
+require_once "util/config.php";
+require_once "util/challenge_check_config.php";
+
+$conn = new mysqli($servername, $chalcheck_username, $chalcheck_password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$user_id = $_SESSION['user_id'];
 
 $avatar = ($_SESSION['avatar'] != null) ? $_SESSION['avatar'] : 'https://shorturl.at/fkJM9';
 
@@ -20,6 +30,30 @@ $user_name = isset($_SESSION['name']) ? $_SESSION['name'] : 'User';
 // Check if logged in user is the admin
 $is_admin = isset($_SESSION['user_id']) && $_SESSION['user_id'] == 1;
 
+
+// Function to check if a challenge is solved by the user
+function isChallengeSolved($conn, $user_id, $challenge_id) {
+    $count = null;
+
+    $sql = "SELECT COUNT(*) FROM solved_challenges WHERE user_id = ? AND challenge_id = ?;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ii", $user_id, $challenge_id);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+
+    return $count > 0;
+}
+
+$challenges = [
+    ['id' => 1, 'name' => 'SQLi'],
+    ['id' => 2, 'name' => 'XSS'],
+    ['id' => 3, 'name' => 'Access_Control'],
+    ['id' => 4, 'name' => 'CSRF'],
+    ['id' => 5, 'name' => 'XSS2']
+];
+
 ?>
 
 <!DOCTYPE html>
@@ -27,24 +61,70 @@ $is_admin = isset($_SESSION['user_id']) && $_SESSION['user_id'] == 1;
 <head>
     <title>Challenges</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <style>
+        .container>div {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        img {
+            width: 5rem;
+            height: 5rem;
+            border-radius: 30%;
+        }
+
+        .challenge-item {
+            background-color: lightgray;
+            padding: 0.5rem;
+            border-radius: 0.5rem;
+            margin-bottom: 0.2rem;
+            color: black;
+        }
+
+        .challenge-item:hover {
+            background-color: darkgray;
+        }
+        
+        .solved-challenge {
+            background-color: green;
+            color: white;
+        }
+
+        .solved-challenge:hover {
+            background-color: darkgreen;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
-        <div style="display: flex; justify-content: space-between;">
+        <div>
             <div>
-                <h1>Challenges</h1>
-                <p>Welcome, <?php echo $user_name; ?>!</p>
+                <h2>Challenges</h2>
+                <p>Welcome, <b><?php echo $user_name; ?></b>!</p>
             </div>
-            <img src="<?php echo $avatar ?>" alt="avatar" style="width: 5rem; height: 5rem; border-radius: 30%;">
+            <img src="<?php echo $avatar ?>" alt="avatar">
         </div>
 
-        <h2>Available Challenges</h2>
+        <h3>Available Challenges</h3>
         <ol>
-            <li><a href="challenges/challenge_sqli.php">CHALLENGE 1</a></li>
-            <li><a href="challenges/challenge_xss.php">CHALLANGE 2</a></li>
-            <li><a href="challenges/challenge_access_control.php">CHALLENGE 3</a></li>
-            <li><a href="challenges/challenge_CSRF.php">CHALLENGE 4</a></li>
-            <li><a href="challenges/challenge_xss2.php">CHALLENGE 5</a></li>
+            <?php
+            foreach ($challenges as $challenge) {
+                $challenge_id = $challenge['id'];
+                $challenge_name = $challenge['name'];
+                $isSolved = isChallengeSolved($conn, $user_id, $challenge_id);
+                $class = $isSolved ? 'solved-challenge' : '';
+                ?>
+            
+                <a href="challenges/challenge_<?php echo $challenge_name; ?>.php">
+                    <li class="challenge-item <?php echo $class; ?>">
+                        CHALLENGE <?php echo $challenge_id; ?>
+                    </li>
+                </a>
+            
+            <?php
+            }
+            ?>
         </ol>
 
         <?php if ($is_admin): ?>
@@ -53,7 +133,7 @@ $is_admin = isset($_SESSION['user_id']) && $_SESSION['user_id'] == 1;
             </button>
         <?php endif; ?>
 
-        <p><a href="logout.php">Logout</a></p>
+        <p><a class="button-style" href="logout.php">Logout</a></p>
     </div>
 </body>
 </html>
